e3457c9ad94bc8e2dc83050928e893900c3591cc
Christina <184446177+ckim53@users.noreply.github.com>
2025-10-28 15:47:57 -0700

    Implement sorting posts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import PostCard from '../components/PostCard';
-import { Text, Box, Button, Group } from '@mantine/core';
+import { Text, Box, Button, Group, Select } from '@mantine/core';
 import { useAuth } from '../auth/AuthProvider';
 import { useApiFetch } from '../services/apiFetch';
 import { API_URL } from '../services/api';
@@ -12,6 +12,7 @@ import LogoutButton from '../components/LogoutButton';
 function Dashboard() {
 	const { authorId } = useParams();
 	const [posts, setPosts] = useState([]);
+	const [sortBy, setSortBy] = useState('date');
 	const { user } = useAuth();
 	const apiFetch = useApiFetch();
 	const navigate = useNavigate();
@@ -19,6 +20,14 @@ function Dashboard() {
 	const handleNewPost = () => {
 		navigate(`/admin/${user.id}/posts/new`);
 	};
+
+	const sortedPosts = [...posts].sort((a, b) => {
+		if (sortBy === 'name') return a.title.localeCompare(b.title);
+		if (sortBy === 'date') return new Date(b.createdAt) - new Date(a.createdAt);
+		if (sortBy === 'status')
+			return b.published === a.published ? 0 : b.published ? 1 : -1;
+		return 0;
+	});
 
 	useEffect(() => {
 		apiFetch(`${API_URL}/admin/${authorId}/posts`, {
@@ -33,9 +42,8 @@ function Dashboard() {
 
 	return (
 		<Box px={50}>
-			<Group mt={10} mb={30}>
+			<Group mt={10}>
 				<Text
-					mb={20}
 					style={{
 						color: 'white',
 						fontSize: '40px',
@@ -46,7 +54,7 @@ function Dashboard() {
 				</Text>
 				<IconCoffee
 					style={{
-						marginBottom: '25px',
+						marginBottom: '10px',
 						color: 'white',
 					}}
 					size={45}
@@ -55,16 +63,30 @@ function Dashboard() {
 					onClick={handleNewPost}
 					radius="md"
 					size="md"
-					mb="lg"
 					mx="lg"
 					leftSection={<IconLibraryPlus></IconLibraryPlus>}
 				>
 					New Post
 				</Button>
+
 				<LogoutButton />
 			</Group>
+			<Select
+			shadow="xs"
+				size="md"
+				value={sortBy}
+				onChange={setSortBy}
+				c="white"
+				data={[
+					{ value: 'date', label: 'Date Added (Newest First)' },
+					{ value: 'name', label: 'Title' },
+				]}
+				label="Sort By"
+				maw={250}
+				my="xl"
+			/>
 			<Box className="posts-grid">
-				{posts.map((p) => (
+				{sortedPosts.map((p) => (
 					<PostCard key={p.id} post={p} />
 				))}
 			</Box>
